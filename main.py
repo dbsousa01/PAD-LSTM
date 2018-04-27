ce90ec0c47347bb7c397f4a562ee6df828525a68
dbsousa01 <danielbsousa1@gmail.com>
2018-04-27 17:03:41 +0100

    Mid-process of changning the  neural network

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import copy
 import pathlib
 import time 
 
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+#from keras.applications.imagenet_utils import preprocess_input
+#from imagenet_utils import decode_predictions
 from keras.models import Sequential, Model
 from keras.layers import Input, Dense, Flatten, Dropout, Activation, Lambda, Permute, Reshape
 from keras.layers import Convolution2D, ZeroPadding2D, MaxPooling2D
@@ -132,9 +136,38 @@ def pred(kmodel, crpimg, transform=False):
     best_name = description[best_index,0]
     print(best_index, best_name[0], out[0,best_index], [np.min(out), np.max(out)])
 
-############################################################# TRAINING NN ##################################################
+def replace_intermediate_layer_in_keras(model, layer_id, new_layer):
+    from keras.models import Model
+
+    layers = [l for l in model.layers]
+
+    x = layers[0].output
+    for i in range(1, len(layers)):
+        if i == layer_id:
+            x = new_layer(x)
+        else:
+            x = layers[i](x)
+
+    new_model = Model(input=layers[0].input, output=x)
+    return new_model
+
+def insert_intermediate_layer_in_keras(model, layer_id, new_layer):
+    from keras.models import Model
+
+    layers = [l for l in model.layers]
+
+    x = layers[0].output
+    for i in range(1, len(layers)):
+        if i == layer_id:
+            x = new_layer(x)
+        x = layers[i](x)
+
+    new_model = Model(input=layers[0].input, output=x)
+    return new_model
+
+############################################################ CREATE AND CHANGE THE NEURAL NETWORK #########################################
 # Use this line to run using CPU
-os.environ['CUDA_VISIBLE_DEVICES'] = ''
+#os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
 facemodel = vgg_face_blank()
 #facemodel.summary()
@@ -145,14 +178,62 @@ l = data['layers']
 description = data['meta'][0,0].classes[0,0].description
 
 copy_mat_to_keras(facemodel)
+facemodel.summary()
+
+facemodel = replace_intermediate_layer_in_keras(facemodel, 23,Convolution2D(2, kernel_size=(1, 1), activation='relu', name='fc8') )
+facemodel.add( Activation('softmax') )
+facemodel.summary()
+############################################################# TRAINING DATA ##################################################
 
 # Gets the path for the current working directory
 PATH = os.getcwd()
-count = 0
-for image in glob.glob(PATH + '/Frames/replay/train/**/**/*.jpg', recursive = True):
-	count += 1
+data_real_path = PATH + '/Frames/replay/train/real/**/*.jpg'
+data_attack_path = PATH + '/Frames/replay/train/attack/**/*.jpg'
+img_data_list = []
 
-print(count)
+"""
+for img in glob.glob(data_real_path, recursive = True):
+	img = image.load_img(os.path.realpath(img), target_size=(224,224))
+	x = image.img_to_array(img)
+	x = np.expand_dims(x, axis = 0)
+	x = preprocess_input(x)
+	img_data_list.append(x)
+
+#print(len(img_data_list))
+
+for img in glob.glob(data_attack_path, recursive = True):
+	img = image.load_img(os.path.realpath(img), target_size=(224,224))
+	x = image.img_to_array(img)
+	x = np.expand_dims(x, axis = 0)
+	x = preprocess_input(x)
+	img_data_list.append(x)
+
+#print(len(img_data_list))
+img_data = np.array(img_data_list)
+#img_data = img_data.astype('float32')
+print (img_data.shape)
+img_data=np.rollaxis(img_data,1,0)
+print (img_data.shape)
+img_data=img_data[0]
+print (img_data.shape)
+
+num_classes = 2
+num_of_samples = img_data.shape[0]
+labels = np.ones((num_of_samples),dtype='int64')
+
+labels[0:8999] = 0 #Real
+labels[9000:37199] = 1 #Spoofing Attack
+
+names = ['Real', 'Spoofing Attack']
+#convert class labels to on-hot encoding
+Y = np_utils.to_categorical(labels,num_classes)
+
+#Shuffle the dataset
+x,y = shuffle(img_data,Y,random_state=2)
+#Split the dataset into training set and cross-validation set (80-20)
+X_train, X_test, Y_train, Y_test = train_test_split(x,y,test_size=  0.2, random_state = 2) 
+
+"""
 """
 ############################################ Convert a video to sequence of frames #######################################
 # Opens a video
@@ -214,3 +295,40 @@ for fn in glob(PATH + '/vgg_face_test/*.jpg'):
 	pred(facemodel, crpim, transform=False)
 	#pred(facemodel, crpim, transform=True) Better for low res images (?)
 """
+######################################################################## PLOTS #########################################################
+
+# visualizing losses and accuracy
+"""
+train_loss=hist.history['loss']
+val_loss=hist.history['val_loss']
+train_acc=hist.history['acc']
+val_acc=hist.history['val_acc']
+xc=range(12)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+savefig('plot1.pdf', bbox_inches='tight')
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+savefig('plot2.pdf', bbox_inches='tight')
+
+"""
+
+K.clear_session()
